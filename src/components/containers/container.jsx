a0066e1ac7e99c8617e315ab5008faba17748123
voranzovv <voranzov@gmail.com>
2019-10-31 03:38:41 +1100

    resolved conflicts

--- src/components/containers/container.jsx
+++ src/components/containers/container.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import 'semantic-ui-react';
 import { Grid } from '@material-ui/core';
-<<<<<<< HEAD
-import ProjectAllocation from '../../screens/project/ProjectAllocation';
+// import ProjectAllocation from '../../screens/project/ProjectAllocation';
 import DoughnutChart from './../charts/doughNut';
 import DeveloperDashboard from './../../screens/dashboards/developerDashboard';
 
 
-=======
 import CompanyDetails from '../../screens/company/CompanyDetails';
 
 import ProjectAllocation from '../../screens/project/allocation/ProjectAllocation';
 import { Switch, Route } from 'react-router-dom'
 import EditRole from '../../screens/project/allocation/EditRole';
 import AddEmployee from './../../screens/CompanyAdministaration/AddEmployee';
->>>>>>> 1a81955d44f90299ed85be679db9b17345fec1be
 // container using material Ui
 // container has property 
 //'textAlight' - left, right, center, justified
@@ -58,17 +55,14 @@ export default function Container({ textAlign, collapsed }) {
        
        
         <Grid item xs={collapsed ? 12 : 10} style={useStyles.subContainer}>
-<<<<<<< HEAD
           <ProjectAllocation />
           <DeveloperDashboard/>
-=======
           <Switch>
             <Route exact path="/" component={EditRole} />
             <Route path="/projectAllocation" component={ProjectAllocation} />
             <Route path= "/companyAdministration" component = {AddEmployee}/>
             <Route path= "/companyCompany" component = {CompanyDetails}/>
           </Switch>
->>>>>>> 1a81955d44f90299ed85be679db9b17345fec1be
         </Grid>
       </Grid>
     </div>
