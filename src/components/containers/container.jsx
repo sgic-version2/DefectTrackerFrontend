f3be5e4745fc051a8473372c5464c674d406b8ae
ThulasigaYathukulan <thulasiyathu11@gmail.com>
2019-10-30 13:46:03 +0530

    company admin sample

--- src/components/containers/container.jsx
+++ src/components/containers/container.jsx
@@ -5,6 +5,7 @@ import Model from '../model/submitModel'
 import ProjectAllocation from '../../screens/project/ProjectAllocation';
 import AddEmployee from './../../screens/CompanyAdministaration/AddEmployee';
 import DefectType from '../../screens/CommonConfiguration/DefectType';
+import DoughnutChartDemo from '../../screens/ProductAdministration/DoughNut'
 // container using material Ui
 // container has property 
 //'textAlight' - left, right, center, justified
@@ -46,6 +47,7 @@ export default function Container({ textAlign, collapsed }) {
           {/* <Model open={open} handleClose={handleClose} handleOpen={handleOpen} form={<ProjectAllocation/>} /> */}
         <AddEmployee/>
         {/* <DefectType/> */}
+        {/* <DoughnutChartDemo/> */}
         </Grid>
 
       </Grid>
