8bb05632fadeb13c00a3a46a4c1cde8ab0c41151
kirikaran <kirikarankiri63@gmail.com>
2019-10-31 16:13:21 +0530

    kiri resolved

--- src/screens/Settings/Troubleshoot and Support/Audit Log/Export/Export.jsx
+++ src/screens/Settings/Troubleshoot and Support/Audit Log/Export/Export.jsx
@@ -5,7 +5,7 @@ class Export extends Component {
     
     render() { 
         return ( 
-            <div style={{marginTop:'-45px',marginBottom:'45px'}}>
+            <div style={{marginTop:'-45px',marginBottom:'10px'}}>
                 
                 <Button  href="https://res.cloudinary.com/kiri/raw/upload/v1572277330/Defect_tracker/Audit_b5xo6s.xlsx" color="primary">Export</Button>
 
