902223e8ca85a512c7fea22358e417428fffca0b
MathushanthS <mathushanth.sri@gmail.com>
2019-11-04 14:29:36 +0530

    mathushanth defect-status

--- src/components/containers/container.jsx
+++ src/components/containers/container.jsx
@@ -1,23 +1,17 @@
-//import React, { useState } from 'react';
 import React from 'react';
 import 'semantic-ui-react';
 import { Grid } from '@material-ui/core';
-// import Model from '../model/submitModel'
-import ProjectAllocation from '../../screens/project/ProjectAllocation';
+import ProjectAllocation from '../../screens/project/allocation/ProjectAllocation';
 import { Switch, Route } from 'react-router-dom'
-// import EditRole from '../../screens/project/EditRole';
-// import AddEmployee from './../../screens/CompanyAdministaration/AddEmployee';
-//import DefectType from '../../screens/CommonConfiguration/DefectType';
-//import AddEmployee from './../../screens/employee/AddEmployee';
-import EmployeeTable from '../../screens/employee/EmployeeTable';
+import EditRole from '../../screens/project/allocation/EditRole';
+import DefectList from '../../screens/DefectStatus/DefectList';
 
-// import DoughnutChartDemo from '../../screens/ProductAdministration/DoughNut'
 // container using material Ui
 // container has property 
 //'textAlight' - left, right, center, justified
 
 export default function Container({ textAlign, collapsed }) {
-  // const [open, setOpen] = useState(false);
+  // const [open, setOpen] = useState(true);
 
   // const handleOpen = () => {
   //   setOpen(true);
@@ -32,7 +26,7 @@ export default function Container({ textAlign, collapsed }) {
       marginLeft: `${!collapsed ? '250px' : '100px'}`,
       background: '#fafafa',
       width: '100%',
-      height: 'auto',
+      height: '100vh',
       zIndex: -1000,
       transition: 'left,0.5s',
       overflow: 'auto',
@@ -46,7 +40,6 @@ export default function Container({ textAlign, collapsed }) {
       overflow: 'auto !important',
       marginBottom: '10%'
 
-
     }
   };
 
@@ -55,12 +48,11 @@ export default function Container({ textAlign, collapsed }) {
       <Grid direction="row" container justify="center" alignItems={textAlign ? textAlign : "center"} style={useStyles.gridContainer} spacing={4}>
         <Grid item xs={collapsed ? 12 : 10} style={useStyles.subContainer}>
           <Switch>
-            <Route exact path="/" component=""/>
+            <Route exact path="/" component={EditRole} />
             <Route path="/projectAllocation" component={ProjectAllocation} />
-            <Route path="/companyEmployee" component={EmployeeTable} />
+            <Route path="/defectStatus" component={DefectList} />
           </Switch>
         </Grid>
-
       </Grid>
     </div>
   )
--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -43,7 +43,7 @@ function renderTitle(title) {
         <span>
             {title}
             <a
-                style={{ float: 'left' }}
+                style={{ float: 'right' }}
                 href="https://www.google.com/search?q=antd"
                 target="_blank"
                 rel="noopener noreferrer"
