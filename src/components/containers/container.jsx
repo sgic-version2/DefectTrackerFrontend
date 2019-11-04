5196315f537e8f11a0b425077038eb9dc338831e
unknown <sasee9488@gmail.com>
2019-11-04 14:44:14 +0530

    update

--- src/components/containers/container.jsx
+++ src/components/containers/container.jsx
@@ -1,38 +1,28 @@
-//import React, { useState } from 'react';
 import React from 'react';
 import 'semantic-ui-react';
 import { Grid } from '@material-ui/core';
-// import Model from '../model/submitModel'
-import ProjectAllocation from '../../screens/project/ProjectAllocation';
+import Dashboard from '../../screens/Dashboard/Dashboard';
+import CompanyDetails from '../../screens/company/CompanyDetails';
+import ProjectAllocation from '../../screens/project/allocation/ProjectAllocation';
 import { Switch, Route } from 'react-router-dom'
-// import EditRole from '../../screens/project/EditRole';
-// import AddEmployee from './../../screens/CompanyAdministaration/AddEmployee';
-//import DefectType from '../../screens/CommonConfiguration/DefectType';
-//import AddEmployee from './../../screens/employee/AddEmployee';
-import EmployeeTable from '../../screens/employee/EmployeeTable';
-
-// import DoughnutChartDemo from '../../screens/ProductAdministration/DoughNut'
+import EditRole from '../../screens/project/allocation/EditRole';
+import AddEmployee from './../../screens/CompanyAdministaration/AddEmployee';
+import ViewDefect from '../../screens/Defect/viewDefect';
+import AuditTable from '../../screens/Settings/Troubleshoot and Support/Audit Log/Audit_table/Audit_table'
+import PriorityConfig from '../../screens/general_configuration/priority/PriorityConfig'
 // container using material Ui
 // container has property 
 //'textAlight' - left, right, center, justified
 
 export default function Container({ textAlign, collapsed }) {
-  // const [open, setOpen] = useState(false);
 
-  // const handleOpen = () => {
-  //   setOpen(true);
-  // };
-
-  // const handleClose = () => {
-  //   setOpen(false);
-  // };
   const useStyles = {
     root: {
       marginTop: '5%',
       marginLeft: `${!collapsed ? '250px' : '100px'}`,
       background: '#fafafa',
       width: '100%',
-      height: 'auto',
+      height: '100vh',
       zIndex: -1000,
       transition: 'left,0.5s',
       overflow: 'auto',
@@ -46,7 +36,6 @@ export default function Container({ textAlign, collapsed }) {
       overflow: 'auto !important',
       marginBottom: '10%'
 
-
     }
   };
 
@@ -54,13 +43,19 @@ export default function Container({ textAlign, collapsed }) {
     <div style={useStyles.root}>
       <Grid direction="row" container justify="center" alignItems={textAlign ? textAlign : "center"} style={useStyles.gridContainer} spacing={4}>
         <Grid item xs={collapsed ? 12 : 10} style={useStyles.subContainer}>
-          <Switch>
-            <Route exact path="/" component=""/>
-            <Route path="/projectAllocation" component={ProjectAllocation} />
-            <Route path="/companyEmployee" component={EmployeeTable} />
-          </Switch>
-        </Grid>
 
+          <Switch>
+            <Route exact path="/" component={EditRole} />
+            <Route path="/defectDashboard" component={Dashboard} />
+            <Route path="/projectAllocation" component={ProjectAllocation} />
+            <Route path="/companyAdministration" component={AddEmployee} />
+            <Route path="/companyCompany" component={CompanyDetails} />
+            <Route path="/defect" component={ViewDefect} />
+            <Route path="/auditLog" component={AuditTable} />
+            <Route path="/priority" component={PriorityConfig} />
+          </Switch>
+
+        </Grid>
       </Grid>
     </div>
   )
