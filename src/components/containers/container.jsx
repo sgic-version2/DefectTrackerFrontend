e27febe2378bec01c1def8b4202d4a2f6b660b54
voranzovv <voranzov@gmail.com>
2019-11-09 17:31:18 +1100

    voranzo developer dashborad

--- src/components/containers/container.jsx
+++ src/components/containers/container.jsx
@@ -9,17 +9,18 @@ import { Switch, Route } from 'react-router-dom';
 import EditRole from '../../screens/project/allocation/EditRole';
 import AddEmployee from './../../screens/CompanyAdministaration/AddEmployee';
 import ViewDefect from '../../screens/Defect/viewDefect';
-import AuditTable from '../../screens/Settings/Troubleshoot and Support/Audit Log/Audit_table/Audit_table';
+import AuditTable from '../../screens/settings/Troubleshoot and Support/Audit Log/Audit_table/Audit_table';
 import PriorityConfig from '../../screens/general_configuration/priority/PriorityConfig';
 import AllocationTable from '../../screens/HrAllocate/AllocationTable';
 import EmployeeTable from './../../screens/employee/EmployeeTable';
 import DefectType from '../../screens/CommonConfiguration/DefectConfig/DefectConfic';
 import DefectStatusFlow from '../../screens/WorkFlow/DefectStatus/DefectStatusFlow';
-import TeachLeadPrivilege from '../../screens/Settings/Privilege/TechLeadPrivileges';
+import TeachLeadPrivilege from '../../screens/settings/Privilege/TechLeadPrivileges';
 import ManageProject from '../../screens/ManageProject/ProjectList'
 import Severity from '../../screens/general_configuration/severity/PriorityConfig'
 import ProfileSetting from '../../screens/profile/profile'
 import DefectRolesFlow from '../../screens/WorkFlow/DefectRoles/DefectRolesFlow'
+import DeveloperDashboard from './../../screens/dashboards/developerDashboard';
 // container using material Ui
 // container has property
 //'textAlight' - left, right, center, justified
@@ -77,7 +78,7 @@ export default function Container({ textAlign, collapsed }) {
             <Route path='/defect/severity' component={Severity} />
             <Route path='/defect/profileSetting' component={ProfileSetting} />
             <Route path='/defect/defectRolesFlow' component={DefectRolesFlow} />
-
+            <Route path='/defect/developer' component={DeveloperDashboard} />
           </Switch>
         </Grid>
       </Grid>
