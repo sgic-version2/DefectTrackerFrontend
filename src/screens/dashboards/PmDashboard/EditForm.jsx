d649307556fbc3d886b44e1eba26ad2d917fc2c3
SanthKavi <kavi1015k@gmail.com>
2019-11-10 23:15:55 +1100

    sample

---
+++ src/screens/dashboards/PmDashboard/EditForm.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const EditForm = () => {
+    return (
+        <div>
+            <form className=" "  >
+      <div >
+  
+  <label>Project Name:</label>
+  <input type="text" id="" className="" placeholder="Project Name"></input>
+  <br/>
+  
+  <label>Member</label>
+  <input type="text" id=""  className="" placeholder=" Member"></input> 
+  
+  <br/>
+  
+  
+  <br/>
+  
+  
+  </div>
+  </form>
+        </div>
+    )
+}
+
+export default EditForm
