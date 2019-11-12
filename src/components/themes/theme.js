0c825a165070292e03440f2a43040fd874789a6c
unknown <sasee9488@gmail.com>
2019-11-12 12:29:58 +0530

    updates themes.js

---
+++ src/components/themes/theme.js
@@ -0,0 +1,12 @@
+import BackImageOne from '../../Assets/background.jpg'
+const themes = {
+    backgroundDrakColor: '#1d1e1f',
+    backgroundImageOne: BackImageOne,
+    backgroundLightColor: '#006bd6'
+}
+const FontColors = {
+    colorIntoBackgroundDrakColor: '#e3e3e3',
+    colorIntoBackgroundImageOne: '#e3e3e3',
+    colorIntoBackgroundLightColor:'#0d0c0c'
+}
+export { themes, FontColors };
