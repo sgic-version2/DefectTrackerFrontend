2725f0674110884502db59cfff26497066ce0908
jathurshan <s.jathur93@gmail.com>
2019-11-30 09:18:28 +0530

    Updates

---
+++ precache-manifest.cf18ed9261f16f008f8c2f2ddd10420a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c326ff752f97cae4acb5acb3610f41e4",
+    "url": "/DefectTrackerFrontend/index.html"
+  },
+  {
+    "revision": "573fc2eefd6754691984",
+    "url": "/DefectTrackerFrontend/static/css/2.2b857762.chunk.css"
+  },
+  {
+    "revision": "858d8c85d25d62e42db0",
+    "url": "/DefectTrackerFrontend/static/css/main.7106453d.chunk.css"
+  },
+  {
+    "revision": "573fc2eefd6754691984",
+    "url": "/DefectTrackerFrontend/static/js/2.7a69f907.chunk.js"
+  },
+  {
+    "revision": "858d8c85d25d62e42db0",
+    "url": "/DefectTrackerFrontend/static/js/main.b956d4b2.chunk.js"
+  },
+  {
+    "revision": "765d894364f9cf411755",
+    "url": "/DefectTrackerFrontend/static/js/runtime-main.b41930b3.js"
+  }
+]);
